c7fc08c17f27a449311749363e45720cb06a7729
ZhuravlovM <zhuravlov.mykyta@gmail.com>
2024-01-16 13:45:15 +0200

    home tasks solved and added

---
+++ home/duplicate_zeros.py
@@ -0,0 +1,40 @@
+"""
+You are given list of integers (int).
+Your task in this mission is to duplicate (..., 🍩, ... --> ..., 🍩, 🍩, ...)
+all zeros (think about donuts ;-P) and return the result as any Iterable.
+"""
+
+
+from collections.abc import Iterable
+
+
+def duplicate_zeros(donuts: list[int]) -> Iterable[int]:
+    result = []
+    for num in donuts:
+        result.append(num)
+        if num == 0:
+            result.append(0)
+    return result
+
+
+print("Example:")
+print(list(duplicate_zeros([1, 0, 2, 3, 0, 4, 5, 0])))
+
+# These "asserts" are used for self-checking
+assert list(duplicate_zeros([1, 0, 2, 3, 0, 4, 5, 0])) == [
+    1,
+    0,
+    0,
+    2,
+    3,
+    0,
+    0,
+    4,
+    5,
+    0,
+    0,
+]
+assert list(duplicate_zeros([0, 0, 0, 0])) == [0, 0, 0, 0, 0, 0, 0, 0]
+assert list(duplicate_zeros([100, 10, 0, 101, 1000])) == [100, 10, 0, 0, 101, 1000]
+
+print("The mission is done!")
